6df3bcdbf7c3d89cd610ddfbb44c51f7c5df4e13
Maarten Dings <maarten.dings@cxcompany.com>
2020-01-27 22:14:34 +0100

    adding link to anchor component

--- src/components/Anchor/index.jsx
+++ src/components/Anchor/index.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'hyperstatic'
 import './style.sass'
 import cc from 'classcat'
 
 export default ({href}, children) => (
-    <a href={href} class={cc({
+    <Link to={href} class={cc({
        'c-anchor': true
     })} data-aos="fade-up">{children}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
-    </a>
+    </Link>
 )
