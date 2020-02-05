af43f7905a7442c09b0ed5c5075d18b79d607090
Maarten Dings <maartendings@gmail.com>
2020-02-05 17:29:05 +0100

    removing mixed content from site

--- src/components/Anchor/index.jsx
+++ src/components/Anchor/index.jsx
@@ -6,6 +6,6 @@ export default ({href}, children) => (
     <Link to={href} class={cc({
        'c-anchor': true
     })} data-aos="fade-up">{children}
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+        <svg viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
     </Link>
 )
--- src/components/Serviceblock/index.jsx
+++ src/components/Serviceblock/index.jsx
@@ -11,7 +11,7 @@ const SetPosition = (state) => {
 export default ({to, disabled}, children) => (
     <Link to={to} class="c-service-block" data-aos="zoom-in">
         <span>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M16.172 9l-6.071-6.071 1.414-1.414L20 10l-.707.707-7.778 7.778-1.414-1.414L16.172 11H0V9z"/></svg>
+            <svg viewBox="0 0 20 20"><path d="M16.172 9l-6.071-6.071 1.414-1.414L20 10l-.707.707-7.778 7.778-1.414-1.414L16.172 11H0V9z"/></svg>
             <div>
                 {children}
                 <span class={cc({
