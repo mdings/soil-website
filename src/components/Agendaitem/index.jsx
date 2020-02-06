19e5d21805e7f1a8eb33af00803f8c73f3c7a904
Maarten Dings <maarten.dings@cxcompany.com>
2020-02-06 11:32:58 +0100

    adding agenda

--- src/components/Agendaitem/index.jsx
+++ src/components/Agendaitem/index.jsx
@@ -5,8 +5,8 @@ import Cta from '../../components/Cta'
 
 dateFormat.i18n = {
     dayNames: [
-        'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat',
-        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+        'Zo', 'Ma', 'Di', 'Woe', 'Do', 'Vrij', 'Za',
+        'Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'
     ],
     monthNames: [
         'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
@@ -17,9 +17,9 @@ dateFormat.i18n = {
     ]
 }
 
-const formatDate = date => dateFormat(date, 'd mmmm yyyy')
+const formatDate = date => dateFormat(date, 'dddd  d mmmm yyyy')
 
-export default ({code, date = null, location = null, delay = 0}, children) => (
+export default ({to, code, date = null, location = null, delay = 0}, children) => (
     <div class={cc({
         'c-agenda-item': true,
         'c-agenda-item--t1': code == 'CHA',
@@ -27,15 +27,17 @@ export default ({code, date = null, location = null, delay = 0}, children) => (
         'c-agenda-item--t3': code == 'ATO',
         'c-agenda-item--t4': code == 'COR',
     })} data-aos="fade-up" data-aos-delay={delay} data-aos-anchor=".c-agenda-item--t1">
-        <span class="c-agenda-item__date">
-            {date ? formatDate(date) : 'TBA'}
-        </span>
-        <span class="c-agenda-item__name">
-            {children}
-        </span>
-        <span class="c-agenda-item__location">
-            {location ? location : 'TBA'}
-        </span>
-        <Cta to="/test" disabled={date==null}>{date ? 'Inschrijven' : 'Binnenkort'}</Cta>
+        <div>
+            <span class="c-agenda-item__date">
+                {date ? formatDate(date) : 'Binnenkort'}
+            </span>
+            <span class="c-agenda-item__name">
+                {children}
+            </span>
+            <span class="c-agenda-item__location">
+                {location ? location : ''}
+            </span>
+        </div>
+        <Cta to={to} disabled={date==null}>{date ? 'Inschrijven' : 'Binnenkort'}</Cta>
     </div>
 )
--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -1,7 +1,8 @@
 
 import Column from '../components/Column'
 import Container from '../components/Container'
-
+import Header from '../components/Header'
+import Registerblock from '../components/Registerblock'
 
 import './home.sass'
 import Agendaitem from '../components/Agendaitem'
@@ -13,22 +14,22 @@ export const Init = (state) => ({
 
 export default state => (
     <div class="p-agenda">
+        <Header>
         <Container>
-            <Column width="7">
-                <h1>Volledige agenda</h1>
-            </Column>
-        </Container>
-        <Container>
-            <Column width="8">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat mollitia atque porro temporibus. Illum officiis eum atque natus facere in esse quidem odit rerum, ea deleniti neque commodi, assumenda dicta.</p>
-            </Column>
-        </Container>
+                <Column width="7">
+                    <h1 data-aos="fade-up">Volledige agenda</h1>
+                   <p>Onderstaand tref je alle data aan waarop wij georganiseerde workshops en trainingen geven.</p>
+                   <p>Daarnaast is er altijd de mogelijkheid om een training in-company of op een extnerne locatie te organiseren. Neem <a href="mailto:sayhi@meetsoil.com">contact</a> met ons op voor meer informatie hierover.</p>
+                </Column>
+            </Container>
+                    
+        </Header>
         <section class="agenda">
            <Container>
                 <Column width="12">
-                    <Agendaitem code="CHA" date="3/18/2020" location="Lichttoren Eindhoven">Chatbot Discovery Track</Agendaitem>
-                    <Agendaitem delay="100" code="GOO" date="4/22/2020" location="Lichttoren Eindhoven">Google Assistant Workshop</Agendaitem>
-                    <Agendaitem delay="200"code="CHA" date="4/25/2020" location="Lichttoren Eindhoven">Chatbot Discovery Track</Agendaitem>
+                    <Agendaitem to="https://www.eventbrite.nl/e/92785133833" code="CHA" date="3/18/2020" location="Lichttoren Eindhoven">Chatbot Discovery Track</Agendaitem>
+                    <Agendaitem to="https://www.eventbrite.nl/e/89022784545" delay="100" code="GOO" date="4/16/2020" location="Lichttoren Eindhoven">Google Assistant Workshop (dag 1)</Agendaitem>
+                    <Agendaitem to="https://www.eventbrite.nl/e/89022784545" delay="200"code="GOO" date="4/17/2020" location="Lichttoren Eindhoven">Google Assistant Workshop (dag 2)</Agendaitem>
                     <Agendaitem delay="300" code="ATO">Atomic Design Training</Agendaitem>
                     <Agendaitem delay="400" code="COR">Core Modeling Workshop</Agendaitem>
                 </Column>
